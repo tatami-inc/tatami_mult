34045b8364abdb9ae2d9f94a975f1030858f2b34
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-09-02 20:22:34 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_mult@676a333a23d061eee82e18c5e7912a54ccd60129 🚀

--- docs/search/all_13.js
+++ docs/search/all_13.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['with_20tt_20fetchcontent_20tt_0',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md4',1,'']]],
   ['with_20tt_20find_5fpackage_20tt_1',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]],
-  ['wrap_5fshared_5fptr_2',['wrap_shared_ptr',['https://tatami-inc.github.io/tatami/namespacetatami.html#a7518f5e8e09a6f6d7d3955b8ea286689',1,'tatami']]],
+  ['wrap_5fshared_5fptr_2',['wrap_shared_ptr',['https://tatami-inc.github.io/tatami/namespacetatami.html#ab008bae4e33a408de29e9fc49d0f902c',1,'tatami']]],
   ['wrap_5fshared_5fptr_2ehpp_3',['wrap_shared_ptr.hpp',['https://tatami-inc.github.io/tatami/wrap__shared__ptr_8hpp.html',1,'']]]
 ];
