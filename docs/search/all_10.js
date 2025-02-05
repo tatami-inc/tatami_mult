40450c3efb24bd9742945184d73c6b428ed3185c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-05 19:07:55 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_mult@e1e0e7144e26872280c09e866ef28162524d71fa 🚀

--- docs/search/all_10.js
+++ docs/search/all_10.js
@@ -8,6 +8,7 @@ var searchData=
   ['total_5',['total',['https://tatami-inc.github.io/tatami/classtatami_1_1ConsecutiveOracle.html#aade9b656fa50f1b41f52486a70591d58',1,'tatami::ConsecutiveOracle::total()'],['https://tatami-inc.github.io/tatami/classtatami_1_1FixedVectorOracle.html#aee8f5bfa07d50e1b1689652543342572',1,'tatami::FixedVectorOracle::total()'],['https://tatami-inc.github.io/tatami/classtatami_1_1FixedViewOracle.html#aa858ead22df2b721fd723a9509ac28f9',1,'tatami::FixedViewOracle::total()'],['https://tatami-inc.github.io/tatami/classtatami_1_1Oracle.html#a611f0c44f1b3de3ec065c91ada017808',1,'tatami::Oracle::total()']]],
   ['transpose_6',['transpose',['https://tatami-inc.github.io/tatami/namespacetatami.html#a07c1d1f96ea3a59d9f6106b17873d494',1,'tatami::transpose(const Input_ *input, size_t nrow, size_t ncol, size_t input_stride, Output_ *output, size_t output_stride)'],['https://tatami-inc.github.io/tatami/namespacetatami.html#a86be7b06c9d13b25f9ff04eed87430a7',1,'tatami::transpose(const Input_ *input, size_t nrow, size_t ncol, Output_ *output)']]],
   ['transpose_2ehpp_7',['transpose.hpp',['https://tatami-inc.github.io/tatami/transpose_8hpp.html',1,'']]],
-  ['tt_20fetchcontent_20tt_8',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md4',1,'']]],
-  ['tt_20find_5fpackage_20tt_9',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]]
+  ['tt_8',['tt',['../index.html#autotoc_md4',1,'CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;'],['../index.html#autotoc_md5',1,'CMake with &lt;tt&gt;find_package()&lt;/tt&gt;']]],
+  ['tt_20fetchcontent_20tt_9',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md4',1,'']]],
+  ['tt_20find_5fpackage_20tt_10',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]]
 ];
